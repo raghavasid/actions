e07edab2981eca282d81c2b7617583e3a5068534
raghavasid <115988499+raghavasid@users.noreply.github.com>
2024-09-24 07:34:00 +0000

    commit

--- scripts/python_script.py
+++ scripts/python_script.py
@@ -1,7 +1,14 @@
-# check_pipeline.py
-
-def main():
-    print("Pipeline check: Success! The pipeline is running correctly.")
+import os
+file_path = "/workspaces/actions/dummy.log"
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+        print(f"File '{file_path}' has been deleted successfully.")
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+    # Replace 'your_file.txt' with the path to the file you want to delete
+    delete_file(file_path)
