7897712779a6ef9ea1e041c02981af7d36fdc1d2
raghavasid <115988499+raghavasid@users.noreply.github.com>
2024-09-24 07:38:43 +0000

    updateing script file

--- scripts/python_script.py
+++ scripts/python_script.py
@@ -1,5 +1,7 @@
 import os
+
 file_path = "/workspaces/actions/dummy.log"
+
 def delete_file(file_path):
     try:
         os.remove(file_path)
@@ -10,5 +12,4 @@ def delete_file(file_path):
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    # Replace 'your_file.txt' with the path to the file you want to delete
     delete_file(file_path)
